b8f48e43dc73eccbec0d2539bd0793d29014f45c
SzĂ©csĂ©nyi PĂ©ter <peterSz@topdesk.com>
2021-10-20 13:50:17 +0200

    A futasok listázása

--- src/components/AllRunList.tsx
+++ src/components/AllRunList.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import { Header } from 'semantic-ui-react';
+import { getRuns } from '../Api';
+import { Run } from '../domain/Run';
 
-const AllRunList: React.FC = () => (
-  <div>
-    <Header as="h1">Here will be the runs</Header>
-  </div>
-);
+const AllRunList: React.FC = () => {
+  const runs = getRuns();
+  return (
+    <div>
+      <table className="ui celled table">
+        <thead>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Duration</th>
+          <th>Distance</th>
+        </thead>
+        {runs.map((run : Run) => (
+          <tr>
+            <td>{run.title}</td>
+            <td>{run.description}</td>
+            <td>{`${run.durationInSeconds} s`}</td>
+            <td>{`${run.distanceInMeters} m`}</td>
+          </tr>
+        ))}
+
+      </table>
+    </div>
+  );
+};
 export default AllRunList;
